77f81f839e445555179b0e474f62edc6d5e7d5f6
Erick Ogaro <ogaroerick1998@gmail.com>
2025-08-22 11:49:47 +0300

    Initial Commit

---
+++ ConvexQuickStart/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ConvexQuickStart
+//
+//  Created by BURN Mfg on 22/08/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ConvexQuickStartTests/ConvexQuickStartTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConvexQuickStartTests.swift
+//  ConvexQuickStartTests
+//
+//  Created by BURN Mfg on 22/08/2025.
+//
+
+import Testing
+@testable import ConvexQuickStart
+
+struct ConvexQuickStartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
